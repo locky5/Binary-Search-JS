8e81f09a8167e449ea08a6c23d45afe7e879907b
locky5 <daikevin19@yahoo.com>
2019-12-05 02:36:41 -0500

    basic binary search structure

---
+++ binarysearch.js
@@ -0,0 +1,21 @@
+let arr = [1,2,3,4,5,6,7,8,9,10]
+
+function binarySearch(arr, target) {
+  let low = 0
+  let high = arr.length - 1
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2)
+    console.log('pivot: ' + mid)
+
+    if (arr[mid] === target) {
+      return mid
+    } else if (arr[mid] < target) {
+      low = mid + 1
+    } else {
+      high = mid - 1
+    }
+  }
+}
+
+console.log(binarySearch(arr, 7))
